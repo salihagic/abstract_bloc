5bbedd8459462d39f416daef5db59df38fce690e
Salih Agić <salih.a@onedigital.group>
2024-03-13 15:39:59 +0100

    ## [1.2.28] * Updated dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## [1.2.28] 
+* Updated dependencies
+
 ## [1.2.27] 
 * Updated dependencies
 
--- lib/abstract_form/state_management/abstract_form_cubit.dart
+++ lib/abstract_form/state_management/abstract_form_cubit.dart
@@ -43,11 +43,9 @@ abstract class AbstractFormCubit<S extends AbstractFormBaseState>
     updateState(state.copyWith() as S);
   }
 
-  Future<void> onBeforeSubmit(model) async {}
   Future<Result> onSubmit(model) => throw Exception('onSubmit Not implemented');
   Future<Result> onSubmitEmpty() =>
       throw Exception('onSubmitEmpty Not implemented');
-  Future<void> onAfterSubmit(Result result, model) async {}
 
   Future<void> onSubmitSuccess(Result result) async {
     updateStatus(FormResultStatus.submittingSuccess);
